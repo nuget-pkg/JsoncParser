f554bdc05f2e419e8e14ec4288fa31a59858438a
JavaCommons Technologies <javacommons@gmail.com>
2026-02-09 22:32:34 +0900

    v2026.0209.2232.29

--- _sed.sh
+++ _sed.sh
@@ -6,7 +6,12 @@ cwd=`pwd`
 ts=`date "+%Y.%m%d.%H%M.%S"`
 version="${ts}"
 
-cd $cwd/JsoncParser
-sed -i -e "s/<Version>.*<\/Version>/<Version>${version}<\/Version>/g" JsoncParser.csproj
+current=$(cd $(dirname $0);pwd)
+echo $current
+name=`echo "$current" | sed -e 's/.*\/\([^\/]*\)$/\1/'`
+echo $name
+
+cd $cwd/$name
+sed -i -e "s/<Version>.*<\/Version>/<Version>${version}<\/Version>/g" $name.csproj
 cd $cwd/
 echo ${version}>version.txt
