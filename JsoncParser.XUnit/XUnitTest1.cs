b3adf4578835517e5c875974832e7009853fb08d
javacommons <javacommons@gmail.com>
2026-06-18 01:57:20 +0900

    .

--- JsoncParser.XUnit/XUnitTest1.cs
+++ JsoncParser.XUnit/XUnitTest1.cs
@@ -1,5 +1,4 @@
 ﻿using Xunit;
-using Global;
 using T = Global.EasyObject;
 
 public class XUnitTest1
@@ -8,53 +7,54 @@ public class XUnitTest1
     public XUnitTest1(ITestOutputHelper testOutputHelper)
     {
         Out = testOutputHelper;
-        EasyObject.ClearSettings();
-        EasyObject.ShowDetail = true;
-        Print("Setup() called");
-    }
-    private void Print(object x, string title = null)
-    {
-        Out.WriteLine(EasyObject.ToPrintable(x, title));
+        T.ClearSettings();
+        T.ShowDetail = true;
+        T.EchoRedirector = Out.WriteLine;
+        T.LogRedirector = Out.WriteLine;
+        T.Log("Setup() called");
     }
     private string ToJson(object x, bool indent = false)
     {
-        return EasyObject.FromObject(x).ToJson(indent: indent);
+        return T.FromObject(x).ToJson(indent: indent);
     }
     [Fact]
     public void Test01()
     {
+        T.Pass();
         var o1 = Global.StrictJsonParser.Parse("""
             { "a": 123 }
             """);
-        Print(o1, "o1");
+        T.Log(o1, "o1");
         Assert.Equal("""
             {"a":123}
             """, ToJson(o1));
         T.AssertIdentical("""
             {"a":123}
             """, ToJson(o1));
+        T.Pass();
     }
     [Fact]
     public void Test02()
     {
+        T.Pass();
         var o3 = Global.JsoncParser.Parse("""
             { "a": 123 }
             """);
-        Print(o3, "o3");
+        T.Log(o3, "o3");
         Assert.Equal("""
             {"a":123}
             """, ToJson(o3));
         var o4 = Global.JsoncParser.Parse("""
             { a: 123 }
             """);
-        Print(o4, "o4");
+        T.Log(o4, "o4");
         Assert.Equal("""
             {"a":123}
             """, ToJson(o4));
         var o5 = Global.JsoncParser.Parse("""
             { "a": /*comment*/123 }
             """);
-        Print(o5, "o5");
+        T.Log(o5, "o5");
         Assert.Equal("""
             {"a":123}
             """, ToJson(o5));
@@ -62,16 +62,18 @@ public class XUnitTest1
             { "a": //line comment
               123 }
             """);
-        Print(o6, "o6");
+        T.Log(o6, "o6");
         Assert.Equal("""
             {"a":123}
             """, ToJson(o6));
+        T.Pass();
     }
     [Fact]
     public void Test03()
     {
+        T.Pass();
         var o1 = Global.JsoncParser.Parse("""'helloハロー©'""");
-        Print(o1, "o1");
+        T.Log(o1, "o1");
         var json = new Global.PlainObjectConverter(forceAscii: false).Stringify(o1, false);
         Assert.Equal("""
              "helloハロー©"
@@ -80,6 +82,7 @@ public class XUnitTest1
         Assert.Equal("""
              "hello\u30CF\u30ED\u30FC\u00A9"
              """, json);
+        T.Pass();
     }
     //[Fact]
     //public void Test04()
